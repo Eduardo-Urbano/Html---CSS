de5160de4169b17fa52265a30201b4db26088eff
EduS-1 <eduardosurbano004@gmail.com>
2025-06-26 17:38:48 -0300

    Criei o exercício 022

---
+++ Flexbox e Grid/assets/sript.js
@@ -0,0 +1,16 @@
+let string = 'texto';
+let number = 21;
+let boolean = true ou false; 
+let array= [ 1,2,3];
+let object = {
+    lista:{
+        userOne:{
+            nome: 'Bruno',
+            idade: 21
+        },
+        userTwo:{
+            nome: 'Pedro',
+            idade: 1321
+        }
+    }
+}
